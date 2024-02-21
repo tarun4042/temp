9dcfb5eb6ffdcf1a4d4f06bc6c22496449ad9dc1
Tarun Gupta <84189424+tarun4042@users.noreply.github.com>
2024-02-21 17:38:53 +0530

    Login Page

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelectorAll(".input-group")[0].classList.add("success");
+document.querySelectorAll(".input-group")[1].classList.add("success");
+document.querySelectorAll(".input-group")[1].classList.toggle("success");
+document.querySelectorAll(".input-group")[0].classList.toggle("success");
